3b0a05370e958a4ec19850662aa735200a106d6b
xdoouglasxxxx <xdoouglasxxxx@gmail.com>
2026-08-06 12:47:53 -0300

    Delete src directory

--- src/app/robots.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/painel", "/api"] },
-    sitemap: `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/sitemap.xml`,
-  };
-}
