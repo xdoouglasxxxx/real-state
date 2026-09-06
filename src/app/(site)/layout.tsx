5c12e27cb34d38e4063ebd8f59c738ed21052163
xdoouglasxxxx <xdoouglasxxxx@gmail.com>
2026-09-06 16:11:40 -0300

    feat(seo): JSON-LD RealEstateAgent no layout do site

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,8 +4,18 @@ import { getTenant } from "@/lib/tenant";
 
 export default async function SiteLayout({ children }: { children: React.ReactNode }) {
   const org = await getTenant();
+  
+  const orgLd = {
+    "@context": "https://schema.org",
+    "@type": "RealEstateAgent",
+    name: org.name,
+    ...(org.phone ? { telephone: org.phone } : {}),
+    ...(org.creci ? { identifier: `CRECI ${org.creci}` } : {}),
+  };
+  
   return (
     <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(orgLd) }} />
       <Header orgName={org.name} />
       {children}
       <Footer orgName={org.name} creci={org.creci} phone={org.phone} />
